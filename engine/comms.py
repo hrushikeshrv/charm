63e6a9d66dfb44100bb66da24bddf9f15527febb
Hrushikesh <hrushikeshrv@gmail.com>
2023-05-21 08:36:29 +0530

    Added requirements for venv

--- engine/comms.py
+++ engine/comms.py
@@ -2,3 +2,12 @@
 Communication module for communicating between the Arduino
 controller of the arm connected to a Serial port and the engine.
 """
+
+import serial
+
+
+COM_PORT = 'COM6'
+BAUD_RATE = 9600
+
+
+socket = serial.Serial(COM_PORT, BAUD_RATE)
