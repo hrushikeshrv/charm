be44fc80ad28c96faeaae842e2e4ca2fa13ed5c6
Hrushikesh <hrushikeshrv@gmail.com>
2023-05-21 09:39:01 +0530

    Added stockfish wrapper

--- engine/play.py
+++ engine/play.py
@@ -13,6 +13,7 @@ def handle_exit(f):
             f()
         except KeyboardInterrupt as e:
             print(f'\nDetected {e.__class__.__name__} {e}.')
+            # TODO: Inform arm
             print('Exiting')
             sys.exit(1)
     return wrapper
@@ -32,9 +33,10 @@ def main():
     parser.add_argument(
         '-d',
         '--depth',
-        choices=range(1, 10),
+        choices=range(1, 11),
         default=5,
-        help='Set the search depth.',
+        type=int,
+        help='Set the search depth. Can be between 1 and 10 (inclusive). Recommended depth for default engine is 4 or 5.',
         dest='depth'
     )
 
