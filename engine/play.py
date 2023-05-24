941705a344bffaee4d6ca9be7837faceef48f84d
Hrushikesh <hrushikeshrv@gmail.com>
2023-05-24 09:55:18 +0530

    Writing game loop

--- engine/play.py
+++ engine/play.py
@@ -2,6 +2,9 @@ import argparse
 from functools import wraps
 import sys
 
+from chessengine import Board
+from chessengine.utils import clear_lines
+import comms
 import stockfishpy
 
 
@@ -35,10 +38,10 @@ def main():
     parser.add_argument(
         '-d',
         '--depth',
-        choices=range(1, 11),
+        choices=range(1, 16),
         default=5,
         type=int,
-        help='Set the search depth. Can be between 1 and 10 (inclusive). Recommended depth for default engine is 4 or 5.',
+        help='Set the search depth. Can be between 1 and 15 (inclusive). Recommended depth for default engine is 4 or 5.',
         dest='depth'
     )
     parser.add_argument(
@@ -50,11 +53,36 @@ def main():
     )
 
     args = parser.parse_args()
-    if args.engine == 'default':
-        pass
-    else:
-        engine = stockfishpy.Engine(args.path)
-        print(engine.isready())
+
+    player_side = input('Do you want to play white or black (w/b)?')
+    while player_side not in ['w', 'b']:
+        print('Enter "w" for white and "b" for black.')
+        player_side = input('Do you want to play white or black (w/b)?')
+
+    player_side = 'white' if player_side == 'w' else 'black'
+    board_side = 'black' if player_side == 'white' else 'black'
+
+    board = Board(board_side)
+    engine = stockfishpy.Engine(args.path)
+    print(engine.isready())
+    
+    side_to_move = player_side if player_side == 'white' else board_side
+    lines_printed = 11
+    while True:
+        if side_to_move == board.side:
+            clear_lines(lines_printed)
+            print(board)
+            lines_printed = 11
+            # Find move and make it (send it to arm)
+            if args.engine == 'default':
+                _, best_move = board.search_forward(args.depth)
+                board.move(best_move[0], best_move[1])
+                comms.send_move_to_arm((best_move[0], best_move[1]))
+            else:
+                pass
+        else:
+            # Read move to make from serial port
+            pass
 
 
 if __name__ == '__main__':
