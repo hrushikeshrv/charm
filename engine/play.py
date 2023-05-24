915ddb45315de30b1b0186be2a890cc321ba2a2e
Hrushikesh <hrushikeshrv@gmail.com>
2023-05-24 12:22:40 +0530

    Added port and baud options to cli entry

--- engine/play.py
+++ engine/play.py
@@ -5,6 +5,7 @@ import sys
 from chessengine import Board
 from chessengine.utils import clear_lines
 import comms
+import serial
 import stockfishpy
 
 
@@ -48,11 +49,28 @@ def main():
         '-p',
         '--path',
         default='stockfish/stockfish.exe',
-        help='Sets the path to the stockfish executable',
+        help='Set the path to the stockfish executable',
         dest='path'
+    )
+    parser.add_argument(
+        '-c',
+        '--port',
+        required=True,
+        help='Set the port name that the arm\'s microcontroller is connected to',
+        dest='port'
+    )
+    parser.add_argument(
+        '-b',
+        '--baud-rate',
+        default=9600,
+        type=int,
+        help='Set the baud rate for communication with Arduino',
+        dest='baud'
     )
 
     args = parser.parse_args()
+    
+    socket = comms.get_socket(args.port, args.baud)
 
     player_side = input('Do you want to play white or black (w/b)?')
     while player_side not in ['w', 'b']:
@@ -64,25 +82,32 @@ def main():
 
     board = Board(board_side)
     engine = stockfishpy.Engine(args.path)
+    engine.ucinewgame()
     print(engine.isready())
     
     side_to_move = player_side if player_side == 'white' else board_side
     lines_printed = 11
+    print(board)
     while True:
+        clear_lines(lines_printed)
+        print(board)
+        lines_printed = 11
         if side_to_move == board.side:
-            clear_lines(lines_printed)
-            print(board)
-            lines_printed = 11
             # Find move and make it (send it to arm)
             if args.engine == 'default':
                 _, best_move = board.search_forward(args.depth)
                 board.move(best_move[0], best_move[1])
-                comms.send_move_to_arm((best_move[0], best_move[1]))
+                comms.send_move_to_arm(socket, (best_move[0], best_move[1]))
             else:
-                pass
+                # Stockfish always makes the best moves
+                best_move = engine.bestmove()['bestmove']
+                engine.setposition([best_move])
+                comms.send_move_to_arm(socket, (best_move[0:2], best_move[2:]))
         else:
             # Read move to make from serial port
             pass
+            
+        side_to_move = 'white' if side_to_move == 'black' else 'black'
 
 
 if __name__ == '__main__':
