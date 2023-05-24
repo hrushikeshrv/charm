da67819d401f8e8675818d6d6b5d476dbe43d3ee
Hrushikesh <hrushikeshrv@gmail.com>
2023-05-24 09:55:35 +0530

    Added send_move_to_arm

--- engine/comms.py
+++ engine/comms.py
@@ -2,8 +2,10 @@
 Communication module for communicating between the Arduino
 controller of the arm connected to a Serial port and the engine.
 """
+from math import log2
 
 import serial
+from chessengine.lookup_tables import pos_to_coords
 
 
 COM_PORT = 'COM6'
@@ -11,3 +13,18 @@ BAUD_RATE = 9600
 
 
 socket = serial.Serial(COM_PORT, BAUD_RATE)
+
+
+def send_move_to_arm(move: tuple[str,str] | tuple[int,int]) -> None:
+    """
+    Encodes the move to bytes and sends move to arm.
+    
+    :param move: A 2-tuple of strings or ints
+    """
+    if type(move[0]) == 'int':
+        _ = (pos_to_coords[int(log2(move[0]))].lower(), pos_to_coords[int(log2(move[1]))].lower())
+    else:
+        _ = (move[0], move[1])
+
+    move_str = f'{_[0]},{_[1]}'
+    socket.write(move_str.encode())
