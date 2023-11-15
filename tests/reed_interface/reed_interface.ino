245f29ae927535206eec089b62de0b5ab92e6372
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2023-11-15 09:57:24 +0530

    Testing reed switch interfacing

---
+++ tests/reed_interface/reed_interface.ino
@@ -0,0 +1,20 @@
+const int REED_PIN = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(REED_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int triggered = digitalRead(REED_PIN);
+
+  if (triggered == LOW) {
+    Serial.println("Switch closed");
+  }
+  else {
+    Serial.println("Switch open");
+  }
+  delay(1000);
+}
